7e3912db8ad33b4f29f7b0f9de1b54d7801382f9
rmn5124 <rmn5124@gmail.com>
2022-09-03 23:59:51 +0530

    first commit

---
+++ src/Gallery.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+import "./gallery.css";
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
+import img5 from "./images/img5.jpg";
+import img6 from "./images/img6.jpg";
+import img7 from "./images/img7.jpg";
+import img8 from "./images/img8.jpg";
+import img9 from "./images/img9.jpg";
+import img10 from "./images/img10.jpg";
+import img11 from "./images/img11.jpg";
+import img12 from "./images/img12.jpg";
+
+const Gallery = () => {
+  let data = [
+    {
+      id: 1,
+      imgSrc: img1,
+    },
+    {
+      id: 2,
+      imgSrc: img2,
+    },
+    {
+      id: 3,
+      imgSrc: img3,
+    },
+    {
+      id: 4,
+      imgSrc: img4,
+    },
+    {
+      id: 5,
+      imgSrc: img5,
+    },
+    {
+      id: 6,
+      imgSrc: img6,
+    },
+    {
+      id: 7,
+      imgSrc: img7,
+    },
+    {
+      id: 8,
+      imgSrc: img8,
+    },
+    {
+      id: 9,
+      imgSrc: img9,
+    },
+    {
+      id: 10,
+      imgSrc: img10,
+    },
+    {
+      id: 11,
+      imgSrc: img11,
+    },
+    {
+      id: 12,
+      imgSrc: img12,
+    },
+  ];
+
+  return (
+    <>
+      <div className="gallery">
+        {data.map((item, index) => {
+          return (
+            <div className="pics" key={index}>
+              <img src={item.imgSrc} style={{ width: "100%" }} alt="" />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+export default Gallery;
